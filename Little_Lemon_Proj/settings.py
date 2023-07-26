2bfc844526f52139caffcef5b09b7cae17eb8d0f
dee-do <dhanashree.desai@gmail.com>
2023-07-26 13:34:39 -0700

    static content exercise commit

--- Little_Lemon_Proj/settings.py
+++ Little_Lemon_Proj/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Little_Lemon_Proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
